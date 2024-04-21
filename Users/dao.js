30b4b799ce3f92e854774387b8a262eeb6fc2cbd
Aidan Matrician <aidanmatrician@gmail.com>
2024-04-20 21:55:11 -0400

    fix

--- Users/dao.js
+++ Users/dao.js
@@ -8,7 +8,7 @@ export const findUserById = (userId) => model.findById(userId);
 export const findUserByUsername = (username) =>
   model.findOne({ username: username });
 export const findUserByCredentials = (username, password) =>
-  model.findOne({ username, password });
+  model.findOne({ username: username, password: password });
 export const updateUser = (userId, user) =>
   model.updateOne({ _id: userId }, { $set: user });
 export const deleteUser = (userId) => model.deleteOne({ _id: userId });
