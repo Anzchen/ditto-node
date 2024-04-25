012be1f79f5243af25b9926daf4dbbe79192d81e
Aidan Matrician <aidanmatrician@gmail.com>
2024-04-24 22:30:06 -0400

    await

--- Reviews/dao.js
+++ Reviews/dao.js
@@ -5,8 +5,8 @@ export const createReview = (review) => {
 };
 export const findAllReviews = () => model.find();
 export const findReviewBySongId = (songId) => model.find({ song_id: songId });
-export const findReviewByReviewer = (username) =>
-  model.findOne({ reviewer: username });
+export const findReviewByReviewer = async (username) =>
+  await model.findOne({ reviewer: username });
 export const updateReview = (reviewId, review) =>
   model.updateOne({ _id: reviewId }, { $set: review });
 export const deleteReview = (reviewId) => model.deleteOne({ _id: reviewId });
